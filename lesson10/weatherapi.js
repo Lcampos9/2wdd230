791a021f0278f76e5adbfd9debaa033a6d00219d
lcampos9 <cam21004@byui.edu>
2022-11-18 11:50:05 -0600

    commit

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -2,7 +2,7 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/3.0/onecall?lat=64.8378&lon=-147.7164&q=Fairbanks&units=imperial&appid=cd9ad916dfac774d08d7341bb3880649';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=cd9ad916dfac774d08d7341bb3880649';
 
 async function apiFetch() {
     try {
